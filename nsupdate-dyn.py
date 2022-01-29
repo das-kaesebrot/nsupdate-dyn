e5028e643e03c00e85c590d05be72c502c31144c
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2022-01-29 19:04:22 +0100

    add: update part, logging

--- nsupdate-dyn.py
+++ nsupdate-dyn.py
@@ -3,11 +3,13 @@ import os
 import sys
 import subprocess
 import json
+from time import sleep
 import requests
 import logging
 
 def genConfig(absDir, confDir, confFileName):
     config = {
+        "interval": 300,
         "ip-resolver": "http://ifconfig.co/ip",
         "key": "/path/to/your/key.conf",
         "domains-to-update": [
@@ -43,43 +45,101 @@ def checkIfIpHasChangedAndReturnNewIPs(conf):
     }
 
     if "A" in conf.get('update-records'):
-        aRecordOnServer = subprocess.run(["dig", "+short", conf.get("domains-to-update")[0], f"@{conf.get('nameserver')}", "A"]).stdout
-        aaaaRecordOnServer = subprocess.run(["dig", "+short", conf.get("domains-to-update")[0], f"@{conf.get('nameserver')}", "AAAA"]).stdout
-
+        aRecordOnServer = subprocess.run(["dig", "+short", conf.get("domains-to-update")[0], f"@{conf.get('nameserver')}", "A"], capture_output=True)
+        aRecordOnServer = aRecordOnServer.stdout.decode("utf-8").strip()
+        
+    if "AAAA" in conf.get('update-records'):
+        aaaaRecordOnServer = subprocess.run(["dig", "+short", conf.get("domains-to-update")[0], f"@{conf.get('nameserver')}", "AAAA"], capture_output=True)
+        aaaaRecordOnServer = aaaaRecordOnServer.stdout.decode("utf-8").strip()
 
     ipv4Curl = subprocess.run(["curl", "-4", conf.get("ip-resolver")], capture_output=True)
     ipv6Curl = subprocess.run(["curl", "-6", conf.get("ip-resolver")], capture_output=True)
 
     # check if IPv4 is available and has changed
     if ipv4Curl.returncode == 0:
-        returnDict["ipv4"] = ipv4Curl.stdout.decode("utf-8").strip()
+        ipv4Local = ipv4Curl.stdout.decode("utf-8").strip()
+        returnDict["ipv4"] = ipv4Local
+        if ipv4Local != aRecordOnServer: returnDict["changed"] = True
 
-    if ipv6Curl.returncode == 1:
-        returnDict["ipv6"] = ipv6Curl.stdout.decode("utf-8").strip()
+    # check if IPv6 is available and has changed
+    if ipv6Curl.returncode == 0:
+        ipv6Local = ipv6Curl.stdout.decode("utf-8").strip()
+        returnDict["ipv6"] = ipv6Local
+        if ipv6Local != aaaaRecordOnServer: returnDict["changed"] = True
 
     return returnDict
 
-def main():
-    absDir = os.path.abspath(os.path.dirname(__file__))
-    confDir = "conf"
-    confFileName = "config.json"
 
-    logging.basicConfig(format='[%(asctime)s] [%(levelname)s] %(message)s', encoding="utf-8", level=logging.DEBUG)
-
-    if not os.path.exists(confDir):
-        genConfig(absDir, confDir, confFileName)
-    
-    conf = readInConf(absDir, confDir, confFileName)
+def generateSTDINForNSUpdate(conf, returnDict):
     newIPv4, newIPv6 = "", ""
 
+    if 'ipv4' in returnDict.keys(): newIPv4 = returnDict.get('ipv4')
+    if 'ipv6' in returnDict.keys(): newIPv6 = returnDict.get('ipv6')
+    
+    deleteStatements = ""
+    addStatements = ""
 
-    returnDict = checkIfIpHasChangedAndReturnNewIPs(conf)
-    logging.debug(returnDict)
-    if returnDict.get('changed') == True:
-        if 'ipv4' in returnDict.keys():
-            newIPv4 = returnDict.get('ipv4')
-        if 'ipv6' in returnDict.keys():
-            newIPv6 = returnDict.get('ipv6')
+    if "A" in conf.get('update-records'):
+        for entry in conf.get('domains-to-update'):
+            deleteStatements += f"update delete {entry}. A\n"
+
+    if "AAAA" in conf.get('update-records'):
+        for entry in conf.get('domains-to-update'):
+            deleteStatements += f"update delete {entry}. AAAA\n"
+            
+
+    if "A" in conf.get('update-records') and newIPv4:
+        for entry in conf.get('domains-to-update'):
+            addStatements += f"update add {entry}. 600 A {newIPv4}\n"
+
+    if "AAAA" in conf.get('update-records') and newIPv6:
+        for entry in conf.get('domains-to-update'):
+            addStatements += f"update add {entry}. 600 AAAA {newIPv6}\n"
+
+    stdinStr = f"""server {conf.get('nameserver')}
+zone {conf.get('zone')}
+{deleteStatements}{addStatements}show
+send
+"""
+
+    logging.debug(stdinStr)
+
+    return stdinStr
+
+def runNSUpdate(conf, stdinStr):
+    subprocess.run(["nsupdated", "-k", conf.get('key')], stdin=stdinStr)
+
+def main():
+    logging.basicConfig(format='[%(asctime)s] [%(levelname)s] %(message)s', encoding="utf-8", level=logging.DEBUG)
+
+    try:
+        absDir = os.path.abspath(os.path.dirname(__file__))
+        confDir = "conf"
+        confFileName = "config.json"
+
+        if not os.path.exists(confDir):
+            genConfig(absDir, confDir, confFileName)
+        
+        conf = readInConf(absDir, confDir, confFileName)
+        
+        while True:
+            returnDict = checkIfIpHasChangedAndReturnNewIPs(conf)
+            logging.debug(returnDict)
+            if returnDict.get('changed') == True:
+                logging.info("Detected IP change, updating records")
+                returnDict.pop('changed')
+
+                runNSUpdate(conf, generateSTDINForNSUpdate(conf, returnDict))
+            
+            else:
+                logging.info("No IPs changed since last check")
+            
+            logging.info(f"Sleeping for {conf.get('interval')}s")
+            sleep(conf.get('interval'))
+
+    except KeyboardInterrupt:
+        logging.info("Stopping updater")
+        sys.exit(0)
 
 
 if __name__ == "__main__":
