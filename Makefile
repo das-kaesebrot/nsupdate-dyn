0677de7fd8fe0d133b79c0dbce2d8eb22f9c5d85
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2022-01-30 17:41:26 +0100

    add: use python script for automated systemd install

--- Makefile
+++ Makefile
@@ -1,25 +1,4 @@
 .DEFAULT_GOAL := install
 
-SCRIPT_NAME = nsupdate-dyn
-ROOT_DIR:=$(shell dirname $(realpath $(firstword $(MAKEFILE_LIST))))
-
-define SERVICECONTENT
-[Unit]
-Description=nsupdate-dyn python3 script
-After=network-online.target
-Wants=network-online.target
-
-[Service]
-Type=simple
-ExecStart=/usr/bin/env python3 ${ROOT_DIR}/${SCRIPT_NAME}.py
-
-[Install]
-WantedBy=multi-user.target
-endef
-export SERVICECONTENT
-
 install:
-	touch /etc/systemd/system/${SCRIPT_NAME}.service
-	@echo "$$SERVICECONTENT" > /etc/systemd/system/${SCRIPT_NAME}.service
-	systemctl daemon-reload
-	systemctl enable --now ${SCRIPT_NAME}
+	python3 install2systemd.py
---
+++ install2systemd.py
@@ -0,0 +1,33 @@
+import os
+import subprocess
+import sys
+
+def main():
+    scriptName="nsupdate-dyn"
+    absScriptPath=os.path.join(os.path.abspath(os.path.dirname(__file__)), scriptName + ".py")
+
+    systemdUnitFileContent = f"""
+[Unit]
+Description={scriptName} python3 script
+After=network-online.target
+Wants=network-online.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/env python3 {absScriptPath} --systemd
+
+[Install]
+WantedBy=multi-user.target
+"""
+    with open(os.path.join("/etc/systemd/system", scriptName + ".service"), mode='w') as f:
+        f.write(systemdUnitFileContent)
+    
+    subprocess.run("systemctl", "daemon-reload")
+    subprocess.run("systemctl", "enable", "--now", scriptName)
+
+if __name__ == "__main__":
+    if os.path.exists("/run/systemd/system"):
+        main()
+    else:
+        print("Path /run/systemd/system not found, aborting install")
+        sys.exit(1)
