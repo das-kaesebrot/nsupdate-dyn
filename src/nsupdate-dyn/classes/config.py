48f9c52c907ecb7de6db60d850dc623ca61d8266
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2024-04-15 18:55:08 +0200

    add default values to config

--- src/nsupdate-dyn/classes/config.py
+++ src/nsupdate-dyn/classes/config.py
@@ -1,19 +1,22 @@
-from dataclasses import dataclass, asdict
+from dataclasses import dataclass, asdict, field
+from os.path import isfile
+from os import makedirs
+from pathlib import Path
 import json
 
 
 @dataclass
 class Config:
+    key_file: str = "/absolute/path/to/key/file"
+    domains_to_update: list[str] = field(default_factory=list)
+    zone: str = "example.com"
+    server: str = "ns1.example.com"
     ip_resolver: str = "http://ifconfig.co/ip"
-    key_file: str
-    domains_to_update: list[str] = []
-    zone: str
-    server: str
 
     @staticmethod
     def from_json(config_file: str):
-        if not config_file:
-            config_file = "/etc/nsupdate-dyn/config.json"
+        if not isfile(config_file):
+            Config.generate_json(config_file)
 
         config_dict_ = {}
 
@@ -21,6 +24,20 @@ class Config:
             config_dict_ = json.load(f)
 
         return Config(**config_dict_)
+
+    @staticmethod
+    def generate_json(config_file: str):
+        config_file_path = Path(config_file)
+        
+        if not config_file_path.parent.exists():
+            makedirs(config_file_path.parent.as_posix())
+        
+        with open(config_file, "x") as f:
+            json.dump(Config().as_dict(), fp=f, indent=4)
+
+    def get_dnskey_dict(self) -> dict:
+        with open(self.key_file, "r") as f:
+            return json.load(f)
 
     def as_dict(self) -> dict:
         return asdict(self)
